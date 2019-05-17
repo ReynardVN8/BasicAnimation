875c5708a8e9e6b05e211855236fea297d48d1af
Reynard Vincent Nata <reynardvnata@yahoo.co.id>
2019-05-17 11:35:59 +0700

    Adding Custom Animation

--- BasicAnimation/ViewController.swift
+++ BasicAnimation/ViewController.swift
@@ -9,10 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var play = true
+    var angle = CGFloat(90*Double.pi/180)
+    
+    @IBOutlet weak var ViewAnimate: UIView!
+    @IBOutlet weak var BackgroundView: UIView!
+    
+    @IBAction func PlayButton(_ sender: Any) {
+        if play {
+            UIView.animate(withDuration: 5) {
+                self.ViewAnimate.frame.origin.x = 150
+                self.ViewAnimate.frame.origin.y = 200
+                self.ViewAnimate.backgroundColor =  #colorLiteral(red: 1, green: 0.08448538929, blue: 0, alpha: 1)
+                self.ViewAnimate.transform = CGAffineTransform(scaleX: 2, y: 2)
+                print(self.ViewAnimate.frame)
+                UIView.animate(withDuration: 4, delay: 5, options: UIView.AnimationOptions.init(), animations: {
+                    self.ViewAnimate.transform = CGAffineTransform(rotationAngle: self.angle)
+                    UIView.animate(withDuration: 4, delay: 9, options: UIView.AnimationOptions.init(), animations: {
+                        self.ViewAnimate.center = self.BackgroundView.center
+                    }, completion: nil)
+                }, completion: nil)
+            }
+            play = false
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print(ViewAnimate.frame)
     }
 
 
